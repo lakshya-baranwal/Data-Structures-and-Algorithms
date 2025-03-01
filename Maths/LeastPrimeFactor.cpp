ef14528a0b79540369caecb618e889d89ad6fa8e
Lakshya <162787214+Omnitrixhunter@users.noreply.github.com>
2025-03-01 19:54:59 +0530

    Create LeastPrimeFactor.cpp

---
+++ Maths/LeastPrimeFactor.cpp
@@ -0,0 +1,40 @@
+//The least prime factor of an integer n is the smallest prime number that divides the number.
+//The least prime factor of all even numbers is 2. A prime number is its own least prime factor (as well as its own greatest prime factor).
+//Note: We need to print 1 for 1.
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long ll;
+#define mod 1000000007
+#define modd 998244353;
+
+vector<int> least_prime(100001, 0);
+void leastPrimeFactor() {
+    //Computation of Least Prime Factor upto 1e5
+    least_prime[1] = 1;
+    for (int i = 2; i <= 100000; i++) {
+        if (least_prime[i] == 0) {
+            least_prime[i] = i;
+            if(i > 1000) break;
+            for (int j = i*i; j <= 100000; j += i)
+                if (least_prime[j] == 0)
+                    least_prime[j] = i;
+        }
+    }
+}
+
+void solve() {
+    ll n; cin >> n;
+    cout << n << " : " << least_prime[n] << "\n";
+    //cout.flush();
+}
+
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0); 
+    
+    int t=1; cin >> t;
+    leastPrimeFactor();
+    while(t--) solve();
+    return 0; 
+}
