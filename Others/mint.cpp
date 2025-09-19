2d83ff045e9486b9c25055e5ee5ee7050781b869
Lakshya <162787214+lakshya-baranwal@users.noreply.github.com>
2025-09-19 22:17:32 +0530

    Update mint.cpp

--- Others/mint.cpp
+++ Others/mint.cpp
@@ -1,93 +1,79 @@
 /*
 *  Author: _CrazyForCode_
+*  Modified by : egoistlakshya
 */
 struct mint {
-	int x;
- 
-	mint () { x = 0;}
-	mint (int32_t xx) { x = xx % mod; if (x < 0) x += mod;}
-	mint (long long xx) { x = xx % mod; if (x < 0) x += mod;}
- 
-	int val() {
-		return x;
-	}
-	mint &operator++() {
-		x++;
-		if (x == mod) x = 0;
-		return *this;
-	}
-	mint &operator--() {
-		if (x == 0) x = mod;
-		x--;
-		return *this;
-	}
-	mint operator++(int32_t) {
-		mint result = *this;
-		++*this;
-		return result;
-	}
- 
-	mint operator--(int32_t) {
-		mint result = *this;
-		--*this;
-		return result;
-	}
-	mint& operator+=(const mint &b) {
-		x += b.x;
-		if (x >= mod) x -= mod;
-		return *this;
-	}
-	mint& operator-=(const mint &b) {
-		x -= b.x;
-		if (x < 0) x += mod;
-		return *this;
-	}
-	mint& operator*=(const mint &b) {
-		long long z = x;
-		z *= b.x;
-		z %= mod;
-		x = (int)z;
-		return *this;
-	}
-	mint operator+() const {
-		return *this;
-	}
-	mint operator-() const {
-		return mint() - *this;
-	}
-	mint operator/=(const mint &b) {
-		return *this = *this * b.inv();
-	}
-	mint power(long long n) const {
-		mint ok = *this, r = 1;
-		while (n) {
-			if (n & 1) {
-				r *= ok;
-			}
-			ok *= ok;
-			n >>= 1;
-		}
-		return r;
-	}
-	mint inv() const {
-		return power(mod - 2);
-	}
-	friend mint operator+(const mint& a, const mint& b) { return mint(a) += b;}
-	friend mint operator-(const mint& a, const mint& b) { return mint(a) -= b;}
-	friend mint operator*(const mint& a, const mint& b) { return mint(a) *= b;}
-	friend mint operator/(const mint& a, const mint& b) { return mint(a) /= b;}
-	friend bool operator==(const mint& a, const mint& b) { return a.x == b.x;}
-	friend bool operator!=(const mint& a, const mint& b) { return a.x != b.x;}
-	mint power(mint a, long long n) {
-		return a.power(n);
-	}
-	friend ostream &operator<<(ostream &os, const mint &m) {
-		os << m.x;
-		return os;
-	}
-	explicit operator bool() const {
-		return x != 0;
-	}
+    static int mod;
+    int x;
+
+    mint() : x(0) {}
+    mint(long long v) {
+        v %= mod;
+        if (v < 0) v += mod;
+        x = int(v);
+    }
+
+    static void set_mod(int m) { mod = m; }
+
+    int val() const { return x; }
+
+    // unary ops
+    mint operator+() const { return *this; }
+    mint operator-() const { return mint() - *this; }
+
+    // arithmetic
+    mint& operator+=(const mint& b) {
+        x += b.x;
+        if (x >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator-=(const mint& b) {
+        x -= b.x;
+        if (x < 0) x += mod;
+        return *this;
+    }
+    mint& operator*=(const mint& b) {
+        x = int((__int128)x * b.x % mod);
+        return *this;
+    }
+    mint& operator/=(const mint& b) {
+        return *this *= b.inv();
+    }
+
+    // power & inverse
+    mint power(long long n) const {
+        mint res = 1, a = *this;
+        while (n > 0) {
+            if (n & 1) res *= a;
+            a *= a;
+            n >>= 1;
+        }
+        return res;
+    }
+    mint inv() const {
+        return power(mod - 2); // works if mod is prime
+    }
+
+    // comparison
+    friend bool operator==(const mint& a, const mint& b) { return a.x == b.x; }
+    friend bool operator!=(const mint& a, const mint& b) { return a.x != b.x; }
+
+    // I/O
+    friend std::ostream& operator<<(std::ostream& os, const mint& m) {
+        return os << m.x;
+    }
+    friend std::istream& operator>>(std::istream& is, mint& m) {
+        long long v;
+        is >> v;
+        m = mint(v);
+        return is;
+    }
+
+    // non-member operators
+    friend mint operator+(mint a, const mint& b) { return a += b; }
+    friend mint operator-(mint a, const mint& b) { return a -= b; }
+    friend mint operator*(mint a, const mint& b) { return a *= b; }
+    friend mint operator/(mint a, const mint& b) { return a /= b; }
 };
- 
+int mint::mod = 1000000007;
 // Don't forget to check mod
