28fb86ca79d965fe2885dd5dbdf7f34f5162ce80
Lakshya <162787214+lakshya-baranwal@users.noreply.github.com>
2026-02-10 04:22:22 +0530

    Add Cartesian Tree implementation in C++
    Implemented a Cartesian Tree structure with initialization based on input vector.

---
+++ Range Query/Cartesian Tree .cpp
@@ -0,0 +1,66 @@
+// Cartesian Tree
+
+// Important properties:
+//  Min-Heap Property 
+//  Inorder traversal of the tree gives back the original order
+//  LCA(i,j) = argmin(a[i..j])
+
+// Each node stores: 
+//  G[i].left   = index of left child
+//  G[i].right  = index of right child
+//  G[i].parent = parent
+
+typedef pair<int, int> P;
+
+struct CartesianTree {
+  struct Node {
+    int left, right, parent;
+    Node(){ left = -1, right = -1, parent = -1; }
+    Node(int l, int r, int p){ left = l, right = r, parent = p; }
+  };
+
+  int root;
+  vector<Node> G;
+
+  CartesianTree(){}
+  CartesianTree(vector<int> a, int n){ init(a, n); }
+
+  int init(vector<int> a, int n) {
+    // G[1..n] is an cartesian tree for input a[1..n]; return the ID of root
+    vector<int> lvec(n+1), rvec(n+1);
+
+    stack<P> stk;
+    stk.push(P(-inf, 0));
+
+    // flip the inequalities for converting between min-max ct
+    for(int i = 1; i <= n; i++){
+      while(stk.top().first > a[i]) stk.pop();
+      lvec[i] = stk.top().second;
+      stk.push(P(a[i], i));
+    }
+    while(stk.size()) stk.pop();
+
+    stk.push(P(-inf, n+1));
+    for(int i = n; i >= 1; i--){
+      while(stk.top().first >= a[i]) stk.pop();
+      rvec[i] = stk.top().second;
+      stk.push(P(a[i], i));
+    }
+
+    G.clear();
+    G.resize(n+1);
+    
+    for(int i = 1; i <= n; i++){
+      int l = -inf, r = -inf;
+      if(lvec[i] >= 1) l = a[lvec[i]];
+      if(rvec[i] <= n) r = a[rvec[i]];
+      if(lvec[i] < 1 and rvec[i] > n) {
+        G[i].parent = -1, root = i;
+        continue;
+      }
+      if(l > r) G[lvec[i]].right = i, G[i].parent = lvec[i];
+      else G[rvec[i]].left = i, G[i].parent = rvec[i];
+    }
+    return root;
+  }
+};
