3e77e9eb02863ac7a3a3ae4945d03f97a615a4a3
Lakshya <162787214+lakshya-baranwal@users.noreply.github.com>
2026-02-09 14:43:21 +0530

    Add Kendall-Tau distance calculation for two permutation
    Implement Kendall-Tau distance calculation to determine the minimum number of adjacent swaps needed to transform one permutation into another.

---
+++ Important Techniques/kendall_tau_distance_permuatation .cpp
@@ -0,0 +1,22 @@
+// Kendall-Tau Distance
+// Minimum number of adjacent swaps needed to transform permutation b into permutation a.
+int dis(vector<int> a, vector<int> b){
+   int l = a.size()-1;
+   vector<int> pos(l+1);
+   for(int i = 1; i <= l; i++){
+      pos[a[i]] = i;
+   }
+   for(int i = 1; i <= l; i++){
+      b[i] = pos[b[i]];
+   }
+
+   int cost = 0;
+   // count number of inversions in b
+   // can be done in nlogn using fenwick tree 
+   for(int i = 1; i <= l; i++){
+      for(int j = 1; j < i; j++){
+          if(b[j] > b[i]) cost++;
+      }
+   }
+   return cost;
+}
