40aa054aa8ec976fab5268f38569267429507279
Lakshya <162787214+lakshya-baranwal@users.noreply.github.com>
2026-02-09 14:47:53 +0530

    Update kendall_tau_distance_permuatation .cpp

--- Important Techniques/kendall_tau_distance_permuatation .cpp
+++ Important Techniques/kendall_tau_distance_permuatation .cpp
@@ -13,6 +13,7 @@ int dis(vector<int> a, vector<int> b){
    int cost = 0;
    // count number of inversions in b
    // can be done in nlogn using fenwick tree 
+   // each adjacent swap fixes atmost one swap
    for(int i = 1; i <= l; i++){
       for(int j = 1; j < i; j++){
           if(b[j] > b[i]) cost++;
