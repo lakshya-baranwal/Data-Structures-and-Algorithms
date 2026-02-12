650c58353d42422aa2da48344563023a4d98b467
Lakshya <162787214+lakshya-baranwal@users.noreply.github.com>
2026-02-12 13:10:07 +0530

    Convert Cartesian Tree to support max heap
    Modified Cartesian Tree implementation to support max heap by adjusting comparisons and sentinel values.

--- Range Query/Cartesian Tree .cpp
+++ Range Query/Cartesian Tree .cpp
@@ -10,6 +10,7 @@
 //  G[i].right  = index of right child
 //  G[i].parent = parent
 
+// we can do minor changes to convert min heap to max heap
 typedef pair<int, int> P;
 
 struct CartesianTree {
@@ -30,10 +31,10 @@ struct CartesianTree {
     vector<int> lvec(n+1), rvec(n+1);
 
     stack<P> stk;
-    stk.push(P(-inf, 0));
-
-    // flip the inequalities for converting between min-max ct
+    stk.push(P(-inf, 0)); // change sentinel to +inf for max heap
+    
     for(int i = 1; i <= n; i++){
+      // change > to < for max heap
       while(stk.top().first > a[i]) stk.pop();
       lvec[i] = stk.top().second;
       stk.push(P(a[i], i));
@@ -42,6 +43,7 @@ struct CartesianTree {
 
     stk.push(P(-inf, n+1));
     for(int i = n; i >= 1; i--){
+      // change >= to <= for max heap
       while(stk.top().first >= a[i]) stk.pop();
       rvec[i] = stk.top().second;
       stk.push(P(a[i], i));
@@ -51,6 +53,7 @@ struct CartesianTree {
     G.resize(n+1);
     
     for(int i = 1; i <= n; i++){
+      // initialize local bounds to +inf
       int l = -inf, r = -inf;
       if(lvec[i] >= 1) l = a[lvec[i]];
       if(rvec[i] <= n) r = a[rvec[i]];
@@ -58,6 +61,9 @@ struct CartesianTree {
         G[i].parent = -1, root = i;
         continue;
       }
+      // change > to < for max heap
+      // why change: choose the smaller neighbour as a parent
+      // because in max heap, smaller values are deeper/closer to children
       if(l > r) G[lvec[i]].right = i, G[i].parent = lvec[i];
       else G[rvec[i]].left = i, G[i].parent = rvec[i];
     }
