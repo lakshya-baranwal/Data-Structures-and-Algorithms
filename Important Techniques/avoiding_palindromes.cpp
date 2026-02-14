9c6f2b691a89fce192651beb762fceaa22b98e95
Lakshya <162787214+lakshya-baranwal@users.noreply.github.com>
2026-02-14 10:24:45 +0530

    Add notes on avoiding palindromes in C++
    Added explanation on avoiding palindromes using distinct integers.

---
+++ Important Techniques/avoiding_palindromes.cpp
@@ -0,0 +1,3 @@
+// It is sufficient to choose three distinct integers x, y, and z and repeat them cyclically to avoid forming palindromes.
+// This works because at the core of all long palindromes, there is always a smaller palindrome of length two (even) or three (odd).
+// And it is mathematically impossible to avoid length one palindromes (single element).
