3b015fd822a1ed530a02c59021fd8a8aad1d1318
Lakshya <162787214+lakshya-baranwal@users.noreply.github.com>
2026-02-23 15:23:51 +0530

    Create 0-1 bfs .cpp

---
+++ Graphs/0-1 bfs .cpp
@@ -0,0 +1,25 @@
+/*
+ * 0-1 BFS 
+ * ---------------
+ * Computes shortest paths from a source in a graph where edge weights are 0 or 1.
+ * Time Complexity: O(|E|)
+ */
+vector<int> d(n, INF);
+d[s] = 0;
+deque<int> q;
+q.push_front(s);
+while (!q.empty()) {
+    int v = q.front();
+    q.pop_front();
+    for (auto edge : adj[v]) {
+        int u = edge.first;
+        int w = edge.second;
+        if (d[v] + w < d[u]) {
+            d[u] = d[v] + w;
+            if (w == 1)
+                q.push_back(u);
+            else
+                q.push_front(u);
+        }
+    }
+}
